d8e81df636848c5cde3f9f0bfcca0ebbee7bbad3
Austin <austinpaul606@gmail.com>
2022-11-05 10:36:30 +0100

    login

--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,21 @@
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import RemoveRedEyeOutlinedIcon from "@mui/icons-material/RemoveRedEyeOutlined";
+import { useState } from "react";
 const Login = () => {
+  const [passwordShown, setPasswordShown] = useState(false);
+
+  // Password toggle handler
+  const togglePassword = () => {
+    // When the handler is invoked
+    // inverse the boolean state of passwordShown
+    setPasswordShown(!passwordShown);
+  };
   return (
     <div className={styles.login}>
       <div className={styles.header__section}>
         <h2>Welcome!</h2>
-        <p>Login to your Account to continue...</p>
+        <p>Log in to your Account to continue...</p>
       </div>
       <div className={styles.image__section}>
         <div className={styles.login__image}>
@@ -22,14 +32,20 @@ const Login = () => {
         </div>
         <div className={styles.password}>
           <p>Password?</p>
-          <input type="password" />
+          <input type={passwordShown ? "text" : "password"} />
+          <RemoveRedEyeOutlinedIcon onClick={togglePassword} />
           <br />
-          <Link href="/forgotpassword">forgot password</Link>
+          <Link href="/forgotpassword">Forgot Password ?</Link>
         </div>
       </div>
-      <button>Login</button>
-      <br />
-      <span>Don't have an Account?</span> <Link href="/signup">Sign up</Link>
+
+      <div className={styles.final__section}>
+        <button>Log in</button>
+        <br />
+        <span>
+          Don&#39;t have an Account yet? <Link href="/signup">Sign up</Link>
+        </span>
+      </div>
     </div>
   );
 };
