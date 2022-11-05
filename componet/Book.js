5497bff75d7aee70974b9b3435f0979ed23837ac
Austin <austinpaul606@gmail.com>
2022-11-05 21:50:55 +0100

    books page

--- componet/Book.js
+++ componet/Book.js
@@ -3,7 +3,87 @@ const Book = ({ title }) => {
   return (
     <div className={styles.book__container}>
       <h1>{title}</h1>
-      <div className={styles.book__details}>
+      {/* <div className={styles.book__details}>
+        <div className={styles.book__row}>
+          <div className={styles.image__container}>
+            <img
+              src="https://m.media-amazon.com/images/I/51u8ZRDCVoL._AC_SY780_.jpg"
+              alt=""
+            />
+          </div>
+          <p>Rich Dad poor dad</p>
+          <p>by Rober T kiyoseki</p>
+        </div>
+        <div className={styles.book__row}>
+          <div className={styles.image__container}>
+            <img
+              src="https://m.media-amazon.com/images/I/71ONWR6eXDL.jpg"
+              alt=""
+            />
+          </div>
+          <p>Rich Dad poor dad</p>
+          <p>by Rober T kiyoseki</p>
+        </div>
+        <div className={styles.book__row}>
+          <div className={styles.image__container}>
+            <img
+              src="https://m.media-amazon.com/images/I/81oc6I4sn-L.jpg"
+              alt=""
+            />
+          </div>
+          <p>Rich Dad poor dad</p>
+          <p>by Rober T kiyoseki</p>
+        </div>
+        <div className={styles.book__row}>
+          <div className={styles.image__container}>
+            <img
+              src="https://d28hgpri8am2if.cloudfront.net/book_images/cvr9780743501682_9780743501682_hr.jpg"
+              alt=""
+            />
+          </div>
+          <p>Rich Dad poor dad</p>
+          <p>by Rober T kiyoseki</p>
+        </div>
+        <div className={styles.book__row}>
+          <div className={styles.image__container}>
+            <img
+              src="https://m.media-amazon.com/images/I/51u8ZRDCVoL._AC_SY780_.jpg"
+              alt=""
+            />
+          </div>
+          <p>Rich Dad poor dad</p>
+          <p>by Rober T kiyoseki</p>
+        </div>
+        <div className={styles.book__row}>
+          <div className={styles.image__container}>
+            <img
+              src="https://m.media-amazon.com/images/I/51u8ZRDCVoL._AC_SY780_.jpg"
+              alt=""
+            />
+          </div>
+          <p>Rich Dad poor dad</p>
+          <p>by Rober T kiyoseki</p>
+        </div>
+        <div className={styles.book__row}>
+          <div className={styles.image__container}>
+            <img
+              src="https://m.media-amazon.com/images/I/51u8ZRDCVoL._AC_SY780_.jpg"
+              alt=""
+            />
+          </div>
+          <p>Rich Dad poor dad</p>
+          <p>by Rober T kiyoseki</p>
+        </div>
+        <div className={styles.book__row}>
+          <div className={styles.image__container}>
+            <img
+              src="https://m.media-amazon.com/images/I/51u8ZRDCVoL._AC_SY780_.jpg"
+              alt=""
+            />
+          </div>
+          <p>Rich Dad poor dad</p>
+          <p>by Rober T kiyoseki</p>
+        </div>
         <div className={styles.book__row}>
           <div className={styles.image__container}>
             <img
@@ -54,7 +134,27 @@ const Book = ({ title }) => {
           <p>Rich Dad poor dad</p>
           <p>by Rober T kiyoseki</p>
         </div>
-      </div>
+        <div className={styles.book__row}>
+          <div className={styles.image__container}>
+            <img
+              src="https://m.media-amazon.com/images/I/51u8ZRDCVoL._AC_SY780_.jpg"
+              alt=""
+            />
+          </div>
+          <p>Rich Dad poor dad</p>
+          <p>by Rober T kiyoseki</p>
+        </div>
+        <div className={styles.book__row}>
+          <div className={styles.image__container}>
+            <img
+              src="https://m.media-amazon.com/images/I/51u8ZRDCVoL._AC_SY780_.jpg"
+              alt=""
+            />
+          </div>
+          <p>Rich Dad poor dad</p>
+          <p>by Rober T kiyoseki</p>
+        </div>
+      </div> */}
     </div>
   );
 };
--- pages/books.js
+++ pages/books.js
@@ -1,10 +1,12 @@
-import Book from "../componet/book";
+import Book from "../componet/Book";
 import styles from "../styles/Home.module.css";
+import SearchOutlinedIcon from "@mui/icons-material";
 const Books = () => {
   return (
     <div>
       <div className={styles.books__input__section}>
         <input type="text" placeholder="type Book name or Author her" />
+        <SearchOutlinedIcon />
       </div>
 
       <Book title="Popular" />
