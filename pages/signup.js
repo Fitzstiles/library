9571f537ad7544dea1c0f2fb372d096b31a686ea
Austin <austinpaul606@gmail.com>
2022-11-05 12:56:50 +0100

    sign up page

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,51 @@
-const Signup = () => {
-  return <div>I am the Sign up page</div>;
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+import RemoveRedEyeOutlinedIcon from "@mui/icons-material/RemoveRedEyeOutlined";
+import { useState } from "react";
+const Login = () => {
+  const [passwordShown, setPasswordShown] = useState(false);
+
+  // Password toggle handler
+  const togglePassword = () => {
+    // When the handler is invoked
+    // inverse the boolean state of passwordShown
+    setPasswordShown(!passwordShown);
+  };
+  return (
+    <div className={styles.login}>
+      <div className={styles.header__section}>
+        <h2>Welcome!</h2>
+        <p>Log in to your Account to continue...</p>
+      </div>
+      <div className={styles.image__section}>
+        <div className={styles.login__image}>
+          <img
+            src="https://trengo.com/blog/wp-content/uploads/2020/03/401-1536x1536.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className={styles.input__section}>
+        <div className={styles.email}>
+          <p>Email address</p>
+          <input type="email" />
+        </div>
+        <div className={styles.password}>
+          <p>Password?</p>
+          <input type={passwordShown ? "text" : "password"} />
+          <RemoveRedEyeOutlinedIcon onClick={togglePassword} />
+        </div>
+      </div>
+
+      <div className={styles.final__section}>
+        <button>Sign up</button>
+        <br />
+        <span>
+          Already Have an Account ? <Link href="/login">login</Link>
+        </span>
+      </div>
+    </div>
+  );
 };
 
-export default Signup;
+export default Login;
