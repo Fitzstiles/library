5698a15fd7570df3039fbf91818c4c32c6fd33c0
Austin <austinpaul606@gmail.com>
2022-11-13 15:45:06 +0100

    added search functionality

--- componet/Searched.js
+++ componet/Searched.js
@@ -1,52 +1,37 @@
 import styles from "../componet/Book.module.css";
 import CloseIcon from "@mui/icons-material/Close";
-const SearchedBooks = ({ toggleViewMode, close }) => {
+
+const SearchedBooks = ({ toggleViewMode, close, bookData, search }) => {
   const contentClassName = toggleViewMode ? styles.background : styles.closed;
   return (
     <div className={contentClassName}>
-      <CloseIcon onClick={close} />
-      <div className={styles.Search__book__container}>
-        <div className={styles.searched__books}>
-          <div className={styles.searched_image__container}>
-            <img
-              src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1348956234l/13465.jpg"
-              alt=""
-            />
-          </div>
-          <p>Rich dad poor dad</p>
-          <p>Robert mugabe</p>
-        </div>
-        <div className={styles.searched__books}>
-          <div className={styles.searched_image__container}>
-            <img
-              src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1348956234l/13465.jpg"
-              alt=""
-            />
-          </div>
-          <p>Rich dad poor dad</p>
-          <p>Robert mugabe</p>
-        </div>
-        <div className={styles.searched__books}>
-          <div className={styles.searched_image__container}>
-            <img
-              src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1348956234l/13465.jpg"
-              alt=""
-            />
-          </div>
-          <p>Rich dad poor dad</p>
-          <p>Robert mugabe</p>
-        </div>
-        <div className={styles.searched__books}>
-          <div className={styles.searched_image__container}>
-            <img
-              src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1348956234l/13465.jpg"
-              alt=""
-            />
-          </div>
-          <p>Rich dad poor dad</p>
-          <p>Robert mugabe</p>
-        </div>
+      <div className={styles.icon}>
+        <CloseIcon onClick={close} />
       </div>
+      {search === "" ? (
+        <h1>Search For Books</h1>
+      ) : (
+        <div className={styles.Search__book__container}>
+          {bookData?.map((book) => {
+            const bookimg =
+              book.volumeInfo.imageLinks &&
+              book.volumeInfo.imageLinks.smallThumbnail;
+            const author = book.volumeInfo.authors;
+
+            if (bookimg != undefined) {
+              return (
+                <div key={book.id} className={styles.searched__books}>
+                  <div className={styles.searched_image__container}>
+                    <img src={bookimg} alt="" />
+                  </div>
+                  <p>{book.volumeInfo.title}</p>
+                  <p>{author}</p>
+                </div>
+              );
+            }
+          })}
+        </div>
+      )}
     </div>
   );
 };
--- pages/books.js
+++ pages/books.js
@@ -3,48 +3,62 @@ import styles from "../styles/Home.module.css";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import { useState } from "react";
 import SearchedBooks from "../componet/Searched";
+import axios from "axios";
 
-const Books = ({ data }) => {
+const Books = () => {
   const [search, setSearch] = useState("");
+  const [bookData, setbookData] = useState();
   const [toggleViewMode, setToggleViewMode] = useState(false);
 
   const searchBook = (e) => {
     e.preventDefault();
-    const resetInputField = () => {
-      setSearch("");
-    };
-    resetInputField();
-    if (search === "") {
-      return;
-    } else {
-      alert(search);
-    }
-    // AIzaSyBXKk0SslCsOfRiy9nXDq5LpojvH8UBLvM
+    axios
+      .get(
+        `https://www.googleapis.com/books/v1/volumes?q=${search}&key=AIzaSyBXKk0SslCsOfRiy9nXDq5LpojvH8UBLvM&maxResults=40`
+      )
+      .then((res) => setbookData(res.data.items))
+      .catch((err) => console.log(err));
   };
 
-  const HandleCloseOnClicked = () => setToggleViewMode(false);
+  const HandleCloseOnClicked = () => {
+    setToggleViewMode(false);
+    setSearch("");
+    if (search === "") {
+      return;
+    }
+  };
+
   return (
-    <form onSubmit={searchBook}>
-      <div className={styles.books__input__section}>
-        <input
-          type="text"
-          placeholder="Type Book name or Author here"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          onClick={() => setToggleViewMode(true)}
-        />
-        <SearchOutlinedIcon style={{ color: "#C15B33" }} onClick={searchBook} />
-        <button type="submit">search</button>
+    <div className={styles.book___container}>
+      <form onSubmit={searchBook}>
+        <div className={styles.books__input__section}>
+          <input
+            type="text"
+            placeholder="Type Book name here"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onClick={() => setToggleViewMode(true)}
+          />
+          <SearchOutlinedIcon
+            style={{ color: "#C15B33" }}
+            onClick={searchBook}
+          />
+          <button type="submit">search</button>
+        </div>
+      </form>
+      <div className={styles.options}>
+        <Book title="Popular" />
+        <Book title="Best Selling" />
+        <Book title="Recommeded" />
       </div>
-      <Book title="Popular" />
-      <Book title="Best Selling" />
-      <Book title="Recommeded" />
+
       <SearchedBooks
         toggleViewMode={toggleViewMode}
         close={HandleCloseOnClicked}
+        bookData={bookData}
+        search={search}
       />
-    </form>
+    </div>
   );
 };
-
 export default Books;
