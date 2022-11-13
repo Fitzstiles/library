6d72d8ef6082354c76d082a853c32e047579afac
Austin <austinpaul606@gmail.com>
2022-11-13 15:57:09 +0100

    added search functionality

--- componet/Searched.js
+++ componet/Searched.js
@@ -1,8 +1,10 @@
 import styles from "../componet/Book.module.css";
 import CloseIcon from "@mui/icons-material/Close";
+import Link from "next/link";
 
 const SearchedBooks = ({ toggleViewMode, close, bookData, search }) => {
   const contentClassName = toggleViewMode ? styles.background : styles.closed;
+
   return (
     <div className={contentClassName}>
       <div className={styles.icon}>
@@ -12,24 +14,30 @@ const SearchedBooks = ({ toggleViewMode, close, bookData, search }) => {
         <h1>Search For Books</h1>
       ) : (
         <div className={styles.Search__book__container}>
-          {bookData?.map((book) => {
-            const bookimg =
-              book.volumeInfo.imageLinks &&
-              book.volumeInfo.imageLinks.smallThumbnail;
-            const author = book.volumeInfo.authors;
+          {bookData ? (
+            bookData?.map((book) => {
+              const bookimg =
+                book.volumeInfo.imageLinks &&
+                book.volumeInfo.imageLinks.smallThumbnail;
+              const author = book.volumeInfo.authors;
 
-            if (bookimg != undefined) {
-              return (
-                <div key={book.id} className={styles.searched__books}>
-                  <div className={styles.searched_image__container}>
-                    <img src={bookimg} alt="" />
+              if (bookimg != undefined) {
+                return (
+                  <div key={book.id} className={styles.searched__books}>
+                    <div className={styles.searched_image__container}>
+                      <Link href={book.id}>
+                        <img src={bookimg} alt="" />
+                      </Link>
+                    </div>
+                    <p>{book.volumeInfo.title}</p>
+                    <p>{author}</p>
                   </div>
-                  <p>{book.volumeInfo.title}</p>
-                  <p>{author}</p>
-                </div>
-              );
-            }
-          })}
+                );
+              }
+            })
+          ) : (
+            <p>loading...</p>
+          )}
         </div>
       )}
     </div>
